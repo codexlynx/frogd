821eb6d73f4af4a7e859d19890f5d1555ba8b413
codexlynx <codexlynx@gmail.com>
2016-05-02 18:49:09 +0100

    Logger structure finished

--- APJumper.py
+++ APJumper.py
@@ -1,15 +1,22 @@
 
 from lib.config import Parser
 
-config = './config.cfg'
+import logging
+
+config      = './config.cfg'
+logfile     = './APJumper.log'
 
 class APJumper(object):
 
     def __init__(self):
+        logging.basicConfig(filename = log, level = logging.DEBUG,
+                            format = '%(asctime)s - %(levelname)s - %(message)s')
+
         self.config = Parser(config)
 
     def start(self):
-        print (self.config.getNetworks())
+        #print (self.config.getNetworks())
+        logging.info('Daemon running...')
 
 def main():
     jumper = APJumper()
